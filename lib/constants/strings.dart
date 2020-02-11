494f8ea8f4caac2795440639f3eefc6a4f42aa73
Vuong Tran <minhvuong2810@gmail.com>
2020-02-11 21:14:10 +0700

    Update dns for sentry
    - Update dns for sentry

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -2,8 +2,8 @@ class Strings {
   Strings._();
 
   //General
-  static const String appName = 'Gamer dual Project';
+  static const String appName = 'Flutter Boilerplate Project';
   static const String versionName = '1';
   static const String dnsSentry =
-      'https://05bb6ca6f6364d66a7487be1e8d155ae@sentry.io/1825709';
+      'https://0c840cdc1eba47799bbc5d973647d57a@sentry.io/1800039';
 }
